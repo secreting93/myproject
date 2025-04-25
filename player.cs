9633c8e37cc40b1df00e8f4fec7ea0d8c84f591d
Arseny <secreting93@mail.ru>
2025-04-26 01:15:28 +0300

    Add unit and Player

---
+++ player.cs
@@ -0,0 +1,11 @@
+class Player : Unit
+{
+	private int _points
+	private int _maxPoints;
+	
+	public Player(int maxPoints)
+	{
+		_maxPoints = maxPoints;
+		_points = 0;
+	}
+}
---
+++ unit.cs
@@ -0,0 +1,11 @@
+class Unit : Object
+{
+	private float _health;
+	private float _speed;
+	
+	public Unit(float Health, float Speed)
+	{
+		_health = Health;
+		_speed = Speed;
+	}
+}
